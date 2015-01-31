1308d0f551ce96415505219ea81d09fae2e90b26
aleccool213 <moteygo@gmail.com>
2015-01-31 17:58:23 -0500

    message output logic now in place

--- Node/example_node.js
+++ Node/example_node.js
@@ -1,3 +1,24 @@
+//global varibles
+
+//sentences that are temp
+simpleSentences = function (){
+
+  var sentenceArr = ["Hi", "Bye", "How are you doing?", "What are your plans for today?", "What's up?"]
+  
+  return sentenceArr;
+};
+
+
+tempArray={
+	"Hi":0,
+	"Bye":0,
+	"How are you doing?":0,
+	"What are your plans for today?":0,
+	"What's up?":0
+};
+
+
+//myo connection
 var Myo = require('../template/entry'),
     hub = new Myo.Hub();
 
@@ -18,25 +39,42 @@ hub.on('frame', function(frame) {
 });
 hub.on('pose', function(pose) {
     currentPose = pose;
+    
     switch(currentPose.type) {
         case currentPose.POSE_FIST:
             console.log("i am making a fist");
             break;
         case currentPose.POSE_WAVE_IN:
-            
+            console.log("i am making a wave in");
+            if(tempArray.Hi == 0){
+            	tempArray.Hi = tempArray.Hi + 1	
+            }
+            console.log(tempArray.Hi);
             break;
         case currentPose.POSE_WAVE_OUT:
-            
+            console.log("i am making a wave out");
+            if(tempArray.Hi == 1){
+            	tempArray.Hi = tempArray.Hi + 1
+            }
+            console.log(tempArray.Hi);
+            if(tempArray.Hi == 2){
+            	console.log("Hi");
+       			tempArray.Hi = 0;
+            }
             break;
         case currentPose.POSE_FINGERS_SPREAD:
-           
+           	console.log("i am making a wave in");
             break;
         case currentPose.POSE_TWIST_IN:
-            
+            console.log("i am making a twist in");
             break;
         case currentPose.POSE_NONE:
         default:
-            
+	        if(tempArray.Hi == 2){
+	    		tempArray.Hi = 0;
+	        }
+	        //console.log(tempArray.Hi);
             break;
     }
+
 });
