3913852dd886171748931e4aa1ad0801a00caa15
aleccool213 <moteygo@gmail.com>
2015-01-31 20:26:42 -0500

    All five sentences should work now.

--- Node/example_node.js
+++ Node/example_node.js
@@ -11,11 +11,11 @@ simpleSentences = function (){
 
 //first index is counter for actions, second and third index has to do with the interval between actions
 tempArray={
-	"Hi":[0,0,0], //wave in, wave out: "Hi"
-	"Bye":[0,0,0], //wave out: "Bye"
-	"How":[0,0,0], //fist, fingers spread: "How are you doing?"
-	"What":[0,0,0], //wave in, fist: "What are your plans for today?"
-	"Whats":[0,0,0], //wave out, fist: "What's up?" 
+	"Hi":[0,0], //wave in, wave out: "Hi"
+	"Bye":[0,0], //wave out: "Bye"
+	"How":[0,0], //fist, fingers spread: "How are you doing?"
+	"What":[0,0], //wave in, fist: "What are your plans for the weekend?"
+	"Ilu":[0,0], //wave out, fist: "I Love you?" 
 	"rest":0
 };
 
@@ -43,56 +43,128 @@ hub.on('pose', function(pose) {
     currentPose = pose;
     
     switch(currentPose.type) {
+    	//Fist
         case currentPose.POSE_FIST:
-            console.log("i am making a fist");
-            if(tempArray.Hi[0] == 0){
-            	tempArray.Hi[0] = tempArray.Hi[0] + 1	
-            }
+            console.log("fist");
+
+            /*
+			"How"
+            */
             if(tempArray.How[0] == 0){
-            	tempArray.How[0] = tempArray.How[0] + 1	
+            	tempArray.How[0] = tempArray.How[0] + 1
+            	tempArray.Ho1[1] = tempArray.rest
+            }
+
+            /*
+            "Whats"
+            */
+            if(tempArray.What[0] == 1 && (tempArray.rest - tempArray.What[1] <= 35)){
+            	console.log("What are you plans for the weekend?");
+       			tempArray.What[0] = 0;
+            }
+
+            /*
+            "Ilu"
+            */
+            if(tempArray.Ilu[0] == 1 && (tempArray.rest - tempArray.Ilu[1] <= 35)){
+            	console.log("I love you");
+       			tempArray.Ilu[0] = 0;
             }
             break;
+
+        //Wave in
         case currentPose.POSE_WAVE_IN:
-            console.log("i am making a wave in");
+            console.log("wave in");
+
+            /*
+			"Hi"
+			*/
             if(tempArray.Hi[0] == 0){
             	tempArray.Hi[0] = tempArray.Hi[0] + 1
             	tempArray.Hi[1] = tempArray.rest	
-            	//console.log(tempArray.Hi[0]);
             }
+
+            /*
+			"What"
+			*/
+            if(tempArray.What[0] == 0){
+            	tempArray.What[0] = tempArray.What[0] + 1
+            	tempArray.What[1] = tempArray.rest	
+            }
+
             break;
+
+        //Wave out
         case currentPose.POSE_WAVE_OUT:
-        	tempArray.Hi[2] = tempArray.rest;
-        	//console.log("tempArray.Hi[2] = " + tempArray.Hi[2] + " tempArray.Hi[1] = " + tempArray.Hi[1]);
-        	//console.log(tempArray.Hi[2] - tempArray.Hi[1]);
-            console.log("i am making a wave out");
+  			console.log("wave out");
+  			/*
+			"Bye"
+  			*/
             if(tempArray.Hi[0] == 0){
             	console.log("Bye");
             }
-            if(tempArray.Hi[0] == 1 && (tempArray.Hi[2] - tempArray.Hi[1] <= 35)){
+
+            /*
+        	"Hi"
+        	*/
+            if(tempArray.Hi[0] == 1 && (tempArray.rest - tempArray.Hi[1] <= 35)){
             	console.log("Hi");
        			tempArray.Hi[0] = 0;
-       			console.log(tempArray.Hi[0]);
             }
+
+            /*
+            "Ilu"
+            */
+            if(tempArray.Ilu[0] == 0){
+            	tempArray.Ilu[0] = tempArray.Ilu[0] + 1
+            	tempArray.Ilu[1] = tempArray.rest	
+            }
+
+
             break;
+
+        //Finger spread
         case currentPose.POSE_FINGERS_SPREAD:
-           	console.log("i am making a fingers spreading motion");
-           	if(tempArray.How[0] == 1){
+           	console.log("fingers spreading");
+
+           	/*
+           	"How"
+           	*/
+           	if(tempArray.How[0] == 1 && (tempArray.rest - tempArray.How[1] <= 35)){
             	console.log("How are you doing?");
             	tempArray.How[0] = 0;
             }
            
             break;
+
+        //Twist in
         case currentPose.POSE_TWIST_IN:
-            console.log("i am making a twist in");
+            console.log("twist in");
             break;
+
+
+        //Nothing
         case currentPose.POSE_NONE:
         default:
 	        //console.log("rest")
 	        tempArray.rest = tempArray.rest + 1;
+
+
+	        /*
+	        Check times in all sentence objects and if later than a 35 count, reduce them to zero
+	        */
 	        if(tempArray.Hi[1] < (tempArray.rest - 35)){
 	        	tempArray.Hi[0] = 0;
 	        }
-	        //console.log(tempArray.Hi);
+	        if(tempArray.How[1] < (tempArray.rest - 35)){
+	        	tempArray.How[1] = 0;
+	        }
+			if(tempArray.What[1] < (tempArray.rest - 35)){
+	        	tempArray.What[1] = 0;
+	        }
+	        if(tempArray.Ilu[1] < (tempArray.rest - 35)){
+	        	tempArray.Ilu[1] = 0;
+	        }		     
             break;
     }
 
