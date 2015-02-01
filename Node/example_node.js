8a32aadea2cd12d41acb0fb3a6079088e44008c6
aleccool213 <moteygo@gmail.com>
2015-01-31 22:00:55 -0500

    All five sentences should be working as intended.

--- Node/example_node.js
+++ Node/example_node.js
@@ -1,23 +1,17 @@
 //global varibles
 
-//sentences that are temp
-simpleSentences = function (){
-
-  var sentenceArr = ["Hi", "Bye", "How are you doing?", "What are your plans for today?", "What's up?"]
-  
-  return sentenceArr;
-};
-
-
 //first index is counter for actions, second and third index has to do with the interval between actions
 tempArray={
 	"Hi":[0,0], //wave in, wave out: "Hi"
 	"Bye":[0,0], //wave out: "Bye"
-	"How":[0,0], //fist, fingers spread: "How are you doing?"
+	"How":[0,0], //wave out, wave in: "How are you doing?"
 	"What":[0,0], //wave in, fist: "What are your plans for the weekend?"
 	"Ilu":[0,0], //wave out, fist: "I Love you?" 
-	"rest":0
+	"rest":0,
+	"fistRest":0
 };
+
+outputString = "";
 
 
 //myo connection
@@ -48,27 +42,26 @@ hub.on('pose', function(pose) {
             console.log("fist");
 
             /*
-			"How"
+            "What"
             */
-            if(tempArray.How[0] == 0){
-            	tempArray.How[0] = tempArray.How[0] + 1
-            	tempArray.Ho1[1] = tempArray.rest
-            }
-
-            /*
-            "Whats"
-            */
-            if(tempArray.What[0] == 1 && (tempArray.rest - tempArray.What[1] <= 35)){
+            console.log(tempArray.rest)
+            console.log(tempArray.What[1])
+            if(tempArray.What[0] == 1 && (tempArray.rest - tempArray.What[1] <= 100)){
             	console.log("What are you plans for the weekend?");
+            	outputString = "What are you plans for the weekend?"
        			tempArray.What[0] = 0;
+       			tempArray.What[1] = 0;
             }
 
             /*
             "Ilu"
             */
-            if(tempArray.Ilu[0] == 1 && (tempArray.rest - tempArray.Ilu[1] <= 35)){
+            
+            if(tempArray.Ilu[0] == 1 && (tempArray.rest - tempArray.Ilu[1] <= 100)){
             	console.log("I love you");
+            	outputString = "I love you"
        			tempArray.Ilu[0] = 0;
+       			tempArray.Ilu[1] = 0;
             }
             break;
 
@@ -83,6 +76,10 @@ hub.on('pose', function(pose) {
             	tempArray.Hi[0] = tempArray.Hi[0] + 1
             	tempArray.Hi[1] = tempArray.rest	
             }
+            else{
+            	tempArray.Hi[1] = tempArray.rest
+            }
+
 
             /*
 			"What"
@@ -91,24 +88,43 @@ hub.on('pose', function(pose) {
             	tempArray.What[0] = tempArray.What[0] + 1
             	tempArray.What[1] = tempArray.rest	
             }
+            else{
+            	tempArray.What[1] = tempArray.rest
+            }
+
+            /*
+           	"How"
+           	*/
+           	//console.log("tempArray.rest = " + tempArray.rest)
+           	//console.log("tempArray.How[1] = " + tempArray.How[1])
+           	if(tempArray.How[0] == 1 && (tempArray.rest - tempArray.How[1] <= 100)){
+            	console.log("How are you doing?");
+            	outputString = "How are you doing?"
+            	tempArray.How[0] = 0;
+            	tempArray.How[1] = 0;
+            }
+
+
 
             break;
 
         //Wave out
         case currentPose.POSE_WAVE_OUT:
   			console.log("wave out");
+
   			/*
 			"Bye"
   			*/
-            if(tempArray.Hi[0] == 0){
-            	console.log("Bye");
-            }
+            console.log("Bye");
+            outputString = "Bye"
+  			
 
             /*
         	"Hi"
         	*/
-            if(tempArray.Hi[0] == 1 && (tempArray.rest - tempArray.Hi[1] <= 35)){
+            if(tempArray.Hi[0] == 1 && (tempArray.rest - tempArray.Hi[1] <= 100)){
             	console.log("Hi");
+            	outputString = "Hi"
        			tempArray.Hi[0] = 0;
             }
 
@@ -117,8 +133,27 @@ hub.on('pose', function(pose) {
             */
             if(tempArray.Ilu[0] == 0){
             	tempArray.Ilu[0] = tempArray.Ilu[0] + 1
-            	tempArray.Ilu[1] = tempArray.rest	
+
+            	tempArray.Ilu[1] = tempArray.rest
+            	console.log(tempArray.Ilu[1])
             }
+            else{
+            	tempArray.Ilu[1] = tempArray.rest
+            	console.log(tempArray.Ilu[1])
+            }
+
+            /*
+			"How"
+            */
+            if(tempArray.How[0] == 0){
+            	tempArray.How[0] = tempArray.How[0] + 1
+            	tempArray.How[1] = tempArray.rest;
+            }
+            else{
+            	tempArray.How[1] = tempArray.rest;
+            }
+
+            
 
 
             break;
@@ -127,13 +162,7 @@ hub.on('pose', function(pose) {
         case currentPose.POSE_FINGERS_SPREAD:
            	console.log("fingers spreading");
 
-           	/*
-           	"How"
-           	*/
-           	if(tempArray.How[0] == 1 && (tempArray.rest - tempArray.How[1] <= 35)){
-            	console.log("How are you doing?");
-            	tempArray.How[0] = 0;
-            }
+           	
            
             break;
 
@@ -149,20 +178,19 @@ hub.on('pose', function(pose) {
 	        //console.log("rest")
 	        tempArray.rest = tempArray.rest + 1;
 
-
 	        /*
 	        Check times in all sentence objects and if later than a 35 count, reduce them to zero
 	        */
-	        if(tempArray.Hi[1] < (tempArray.rest - 35)){
+	        if(tempArray.Hi[1] < (tempArray.rest - 100)){
 	        	tempArray.Hi[0] = 0;
 	        }
-	        if(tempArray.How[1] < (tempArray.rest - 35)){
+	        if(tempArray.How[1] < (tempArray.rest - 100)){
 	        	tempArray.How[1] = 0;
 	        }
-			if(tempArray.What[1] < (tempArray.rest - 35)){
+			if(tempArray.What[1] < (tempArray.rest - 100)){
 	        	tempArray.What[1] = 0;
 	        }
-	        if(tempArray.Ilu[1] < (tempArray.rest - 35)){
+	        if(tempArray.Ilu[1] < (tempArray.rest - 100)){
 	        	tempArray.Ilu[1] = 0;
 	        }		     
             break;
